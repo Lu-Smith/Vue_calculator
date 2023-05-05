a3e7469c55d847ae2fc778d233771d647f4d2db3
Luna Smith <artistlunasmith@gmail.com>
2023-05-05 16:25:11 +0100

    Updated FooterApp.vue

--- vue-calculator/src/components/FooterApp.vue
+++ vue-calculator/src/components/FooterApp.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>I am a footer</p>
+  <p>
+    This project was coded by
+    <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a> and
+    is open-source on
+    <a href="https://github.com/Lu-Smith/Vue_calculator" target="_blank"
+      >GitHub</a
+    >.
+  </p>
 </template>
 <script></script>
 <style scoped></style>
