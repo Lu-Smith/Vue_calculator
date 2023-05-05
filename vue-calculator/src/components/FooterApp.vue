bc4ae421b0bd1a55a693a9e3930062983c3e3405
Luna Smith <artistlunasmith@gmail.com>
2023-05-05 16:49:32 +0100

    Updated links style in FooterApp.vue

--- vue-calculator/src/components/FooterApp.vue
+++ vue-calculator/src/components/FooterApp.vue
@@ -1,12 +1,42 @@
 <template>
   <p>
     This project was coded by
-    <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a> and
-    is open-source on
-    <a href="https://github.com/Lu-Smith/Vue_calculator" target="_blank"
+    <a href="https://www.lunasmithart.com/" target="_blank" class="cool-link"
+      >Luna Smith</a
+    >
+    and is open-source on
+    <a
+      href="https://github.com/Lu-Smith/Vue_calculator"
+      target="_blank"
+      class="cool-link"
       >GitHub</a
     >.
   </p>
 </template>
 <script></script>
-<style scoped></style>
+<style scoped>
+.cool-link {
+  position: relative;
+  text-decoration: none;
+  color: #555;
+  transition: all 0.3s ease;
+}
+
+.cool-link:before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: #3fb984;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
+}
+
+.cool-link:hover:before {
+  visibility: visible;
+  transform: scaleX(1);
+}
+</style>
